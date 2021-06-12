81e18cef92c986149e73e1f9bd783f6db420cedc
Tri Phan <phanphutri@gmail.com>
2021-06-12 08:22:08 +0700

    Added 04_Debugging

---
+++ 04_Debugging/script.js
@@ -0,0 +1,39 @@
+// let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+// let len = alphabet.length;
+
+// for (let i = 0; i <= len; i++) {
+//   console.log(alphabet[i]);
+// }
+// console.log('-------------------------------------------------------------');
+
+// for (let i = 1; i < len; i++) {
+//   console.log(alphabet[i]);
+// }
+// console.log('-------------------------------------------------------------');
+
+// for (let i = 0; i < len; i++) {
+//   console.log(alphabet[i]);
+// }
+// --------------------------------------------------------------------------------------------------------
+
+/**
+ * USE CAUTION WHEN REINITIALIZING VARIABLES INSIDE A LOOP
+ */
+
+// function zeroArray(m, n) {
+//   // Creates a 2-D array with m rows and n columns of zeroes
+//   let newArray = [];
+//   for (let i = 0; i < m; i++) {
+//     let arr = [];
+//     for (let k = 0; k < n; k++) {
+//       arr.push(0);
+//     }
+//     newArray.push(arr);
+//   }
+//   return newArray;
+// }
+
+// let array = zeroArray(3, 2);
+// console.log(array);
+// // [[0, 0], [0, 0], [0, 0]]
+// --------------------------------------------------------------------------------------------------------
