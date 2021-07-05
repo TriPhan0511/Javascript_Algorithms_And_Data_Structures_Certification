421b59b1b505f362fd7ead80e5db7f7e65dab6df
Tri Phan <phanphutri@gmail.com>
2021-07-05 19:11:23 +0700

    Updated

--- 08_Functional_Programming/script.js
+++ 08_Functional_Programming/script.js
@@ -1,65 +1,3 @@
-// // Function that returns a string representing a cup of green tea
-// let prepareGreenTea = () => 'greenTea';
-
-// // Function that returns a string that representing a cup of black tea
-// let prepareBlackTea = () => 'blackTea';
-
-// // Given a function (representing the tea type) and the number of cups needed,
-// // the following function returns an array of strings
-// // (each representing a cup of specific type of tea)
-// const getTea = (prepareTea, numOfCups) => {
-//     const teaCups = [];
-//     for (let cups = 1; cups < numOfCups; cups++) {
-//         const teaCup = prepareTea();
-//         teaCups.push(teaCup);
-//     }
-//     return teaCups;
-// };
-
-// const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
-// const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
-
-// console.log(tea4GreenTeamFCC, tea4BlackTeamFCC);
-// --------------------------------------------------------------------------------------------------
-
-// // tabs is an array of titles of each site open within the window
-// var Window = function (tabs) {
-//     this.tabs = tabs; // We keep a record of the array inside the object
-// };
-
-// // When you join two windows into one window
-// Window.prototype.join = function (otherWindow) {
-//     this.tabs = this.tabs.concat(otherWindow.tabs);
-//     return this;
-// };
-
-// // When you open a new tab at the end
-// Window.prototype.tabOpen = function (tab) {
-//     this.tabs.push('new tab');
-//     return this;
-// };
-
-// // When you close a tab
-// Window.prototype.closeTab = function (index) {
-//     var tabsBeforeIndex = this.tabs.splice(0, index); // Get the tabs before the tab
-//     var tabsAfterIndex = this.tabs.splice(index + 1); // Get the tabs after the tab
-//     this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // Join them together
-//     return this;
-// };
-
-// // let array = [1, 2, 3, 4, 5, 6, 7];
-// // let index = 2;
-// // let before = array.splice(0, index);
-// // let after = array.splice(index + 1);
-
-// // console.log(before);
-// // console.log(after);
-
-// let array = [1, 2, 3, 4, 5];
-// console.log(array.slice(0, 2));
-// console.log(array);
-// ------------------------------------------------------------------------------------
-
 /** 01 - Learning about functional programming */
 
 /**
@@ -107,24 +45,75 @@
  *  Callbacks are functions that are slipped or passed into another function to decide the invocation
  *  of that function. You may have seen them passed to other methods, for example in filter,
  *  the callback function tells Javascript the criteria for how to filter an array.
+ *
+ *  Functions that can be assigned to a variable, passed into another function, or returned from another function
+ *  just like any other normal value, are called first class functions.
+ *  In Javascript, all functions are first class functions.
+ *
+ *  The functions that take a function as a argument, or return a function as a return value are called
+ *  higher order functions.
+ *
+ *  When functions are passed in to or returned from another function, then those functions which were passed in
+ *  or returned can be called a lambda.
  */
 
-// Example:
-// Function that returns a string representing a cup of green tea
-const prepareGreenTea = () => 'greenTea';
-// Function that returns a string representing a cup of green tea
-const prepareBlackTea = () => 'blackTea';
+/**
+ * Example:
+ * Prepare 27 cups of green tea and 13 cups of black tea and store them in tea4GreenTeamFCC and tea4BlackTeamFCC
+ * varaibles, respectively. Note that the getTea function has been modified so it now takes a function as
+ * the fisrt argument.
+ */
 
-// Given a function (representing the tea type) and number of cups needed, the following function
-// returns an array of strings (each representing a cup of a specific type of tea)
-const getTea = (prepareTea, numOfCups) => {
-    const teaCups = [];
-    for (let cups = 1; cups <= numOfCups; cups++) {
-        const teaCup = prepareTea();
-        teaCups.push(teaCup);
-    }
-    return teaCups;
-};
+// // Function that returns a string representing a cup of green tea
+// const prepareGreenTea = () => 'greenTea';
+// // Function that returns a string representing a cup of black tea
+// const prepareBlackTea = () => 'blackTea';
 
-const tea4TeamFFC = getTea(40);
-console.log(tea4TeamFFC);
+// // Given a function (representing the tea type) and number of cups needed, the following function
+// // returns an array of strings (each representing a cup of a specific type of tea)
+// const getTea = (prepareTea, numOfCups) => {
+//   const teaCups = [];
+//   for (let cups = 1; cups <= numOfCups; cups++) {
+//     const teaCup = prepareTea();
+//     teaCups.push(teaCup);
+//   }
+//   return teaCups;
+// };
+
+// const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+// const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+// ------------------------------------------------------------------------------------
+
+/** 03 - Understand the hazards of using the imperstive code */
+
+/**
+ * Consider the scenario: you are browsing the web in your browser, and want to track the tabs you have opened.
+ * Let's try to model this using simple object-orientd code.
+ *
+ * A Window object is made of tabs, and you usually have more then one Windown open. The titles of each open
+ * site in each Window object is held in an array. Atfer working in the browser (opening new tabs, mergin windows,
+ * and closing tabs), you want to print the tabs that are still open. Closed tabs are removed from the array and
+ * new tabs (for simplicity) get added to the end of it.
+ */
+
+// // Defines a constructor function
+// let Window = function (tabs) {
+//   this.tabs = tabs; // We keep a record of the array inside the object
+// };
+
+// // When you join two windows into one window
+// Window.prototype.join = function (otherWindow) {
+//   this.tabs = this.tabs.concat(otherWindow.tabs);
+//   return this;
+// };
+
+// // When you open a new tab at the end
+// Window.prototype.tabOpen = function () {
+//   this.tabs.push('new tab');
+//   return this;
+// };
+
+// // When you close a tab
+// Window.prototype.tabClose = function(index){
+
+// }
